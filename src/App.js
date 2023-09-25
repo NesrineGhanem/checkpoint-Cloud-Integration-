6718b1c68c6b805d2f075e0904a13c925022d923
nesrine ghanem <ghnessrine92@gmail.com>
2023-09-25 21:47:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react"
+
+const style = {
+  width: "100%",
+  height: "100%",
+};
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Map
+          initialCenter={{
+            lat: 22.17407,
+            lng: 71.666328,
+          }}
+          google={this.props.google}
+          zoom={14}
+          containerStyle={style}
+        />
+        ;
+      </>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  
+  apiKey: 'AIzaSyDGArUEBa5ns09IA7nt7jP-xfNIUkToFts',
+
+})(App);
+
